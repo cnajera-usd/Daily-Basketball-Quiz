2454e9ca53f25d5dfd06332daee6f69b1de4a2d5
cnajera <cnajera@sandiego.edu>
2024-08-19 13:29:42 -0700

    fixes

--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -37,7 +37,7 @@ const QuizPage = () => {
 
 const handlePreviousQuestion = () => {
     setFeedback("");
-    selectedAnswerIndex(null);
+    setSelectedAnswerIndex(null);
     setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
 };
 
