c620b5e3cde9754aa34a30ff54ee97d6b945187b
cnajera <cnajera@sandiego.edu>
2024-08-30 17:46:53 -0700

    changes

--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/QuizPage.css';
 import quizData from '../data/quizData.json';
-import ScoreModal from '../components/ScoreModal'; // Adjust path as necessary
+import ScoreModal from '../components/ScoreModal';
 import { auth } from '../firebaseConfig';
 import { saveQuizScore } from '../services/quizScoreService';
 import moment from 'moment-timezone';
 import { getDoc, doc, setDoc } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
+import 'moment-timezone/data/packed/latest.json';
 
 const QuizPage = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -15,99 +16,105 @@ const QuizPage = () => {
   const [quizQuestions, setQuizQuestions] = useState([]);
   const [answers, setAnswers] = useState({});
   const [score, setScore] = useState(0);
-  const [totalPossibleScore, setTotalPossibleScore] = useState(0); 
+  const [totalPossibleScore, setTotalPossibleScore] = useState(0);
   const [showScoreModal, setShowScoreModal] = useState(false);
-  const [hasCompletedQuiz, setHasCompletedQuiz] = useState(false); 
-  const [hasAttempted, setHasAttempted] = useState(false);  // New state to track if the quiz has been attempted
+  const [hasCompletedQuiz, setHasCompletedQuiz] = useState(false);
+  const [hasAttempted, setHasAttempted] = useState(false);
 
   useEffect(() => {
-      const fetchQuizData = async () => {
-          try {
-              const today = new Date().toLocaleDateString('en-CA'); 
-              const questions = quizData[today]?.questions || [];
-              setQuizQuestions(questions); 
+    const fetchQuizData = async () => {
+        try {
+            const today = moment.tz('America/Los_Angeles').format('YYYY-MM-DD');
+            console.log(`Calculated Today Date: ${today}`);
 
-              let possibleScore = 0;
-              questions.forEach(question => {
-                  switch (question.difficulty) {
-                      case 'Easy':
-                          possibleScore += 1;
-                          break;
-                      case 'Medium':
-                          possibleScore += 2;
-                          break;
-                      case 'Hard':
-                          possibleScore += 3;
-                          break;
-                      case 'Challenging':
-                          possibleScore += 4;
-                          break;
-                      default:
-                          possibleScore += 1; 
-                  }
-              });
-              setTotalPossibleScore(possibleScore);
+            const userId = auth.currentUser ? auth.currentUser.uid : "anonymous";
+            const docName = `${userId}_${today}`;
+            console.log(`Checking Firestore Document: ${docName}`);
 
-              // Check if the user has already taken the quiz today
-              const userId = auth.currentUser ? auth.currentUser.uid : "anonymous";
-              const attemptDoc = await getDoc(doc(db, 'quizAttempts', `${userId}_${today}`));
-              if (attemptDoc.exists()) {  // Corrected 'exsists' to 'exists'
-                setHasAttempted(true);  // Set the state to true if the quiz was already attempted
+            const questions = quizData[today]?.questions || [];
+            if (questions.length === 0) {
+                console.warn('No questions found for today’s date in quizData.');
+            }
+            setQuizQuestions(questions);
+
+            let possibleScore = 0;
+            questions.forEach(question => {
+                switch (question.difficulty) {
+                    case 'Easy':
+                        possibleScore += 1;
+                        break;
+                    case 'Medium':
+                        possibleScore += 2;
+                        break;
+                    case 'Hard':
+                        possibleScore += 3;
+                        break;
+                    case 'Challenging':
+                        possibleScore += 4;
+                        break;
+                    default:
+                        possibleScore += 1;
+                }
+            });
+            setTotalPossibleScore(possibleScore);
+
+            const attemptDoc = await getDoc(doc(db, 'quizAttempts', docName));
+            console.log(`Quiz Attempt Exists for ${docName}: ${attemptDoc.exists()}`);
+
+            if (attemptDoc.exists()) {
+                setHasAttempted(true);
                 sessionStorage.setItem(`hasAttempted_${userId}_${today}`, 'true');
-              }
-          } catch (error) {
-              console.error('Error fetching quiz data:', error);
-          }
-      };
+            }
+        } catch (error) {
+            console.error('Error fetching quiz data:', error);
+        }
+    };
 
-      // check sessionStorage for 'hasAttempted' to handle page refreshes
-      const userId = auth.currentUser ? auth.currentUser.uid : "anonymous";
-      const today = new Date().toLocaleDateString('en-CA');
-      if (sessionStorage.getItem(`hasAttempted_${userId}_${today}`) === 'true') {
+    const userId = auth.currentUser ? auth.currentUser.uid : "anonymous";
+    const today = moment.tz('America/Los_Angeles').format('YYYY-MM-DD'); // Ensuring consistent date calculation
+
+    const sessionStorageValue = sessionStorage.getItem(`hasAttempted_${userId}_${today}`);
+    console.log(`Session Storage Value: ${sessionStorageValue}`);
+
+    if (sessionStorageValue === 'true') {
         setHasAttempted(true);
-      } else {
-    
-      fetchQuizData();
-      }
-  }, []); // Added empty dependency array to ensure this effect only runs on mount
+    } else {
+        fetchQuizData();
+    }
+}, []); // Ensure this effect only runs on mount
 
-  useEffect(() => {
-    const saveAttempt = async () => {  // Mark the function as async
+useEffect(() => {
+    const saveAttempt = async () => {
         if (hasCompletedQuiz) {
             const userId = auth.currentUser ? auth.currentUser.uid : "anonymous";
             const username = auth.currentUser ? auth.currentUser.displayName || "Unknown User" : "anonymous";
-            const quizDate = moment.tz('America/Los_Angeles').format('YYYY-MM-DD');
+            const quizDate = moment.tz('America/Los_Angeles').format('YYYY-MM-DD'); // Ensuring consistent date calculation
 
-            // Save the quiz attempt to Firestore with the username
             await setDoc(doc(db, 'quizAttempts', `${userId}_${quizDate}`), {
                 userId: userId,
-                username: username, // Include the username here
+                username: username,
                 quizDate: quizDate,
-                timestamp: new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' })
+                timestamp: moment().tz('America/Los_Angeles').format('YYYY-MM-DD HH:mm:ss') // Correct timestamp
             });
 
             saveQuizScore(userId, username, score, totalPossibleScore, quizDate);
-
-            // Set sessionStorage to mark the quiz as attempted
             sessionStorage.setItem(`hasAttempted_${userId}_${quizDate}`, 'true');
         }
     };
 
-    saveAttempt(); // Call the async function
+    saveAttempt();
+}, [hasCompletedQuiz, score, totalPossibleScore]);
 
-    }, [hasCompletedQuiz, score, totalPossibleScore]); // Make sure to include only necessary dependencies
-
-  const handleAnswerClick = (index) => {
+const handleAnswerClick = (index) => {
     setSelectedAnswerIndex(index);
-    setFeedback(null); 
-  };
+    setFeedback(null);
+};
 
-  const handleNextQuestion = () => {
-      if (currentQuestionIndex < quizQuestions.length - 1) {
-          const nextIndex = currentQuestionIndex + 1;
+const handleNextQuestion = () => {
+    if (currentQuestionIndex < quizQuestions.length - 1) {
+        const nextIndex = currentQuestionIndex + 1;
 
-          // Save current answer before moving to next question
-          setAnswers((prevAnswers) => ({
+        setAnswers((prevAnswers) => ({
             ...prevAnswers,
             [currentQuestionIndex]: {
                 selectedAnswerIndex,
@@ -117,142 +124,139 @@ const QuizPage = () => {
 
         setCurrentQuestionIndex(nextIndex);
 
-        // Retrieve and set the previously saved answer for next question
         const nextAnswer = answers[nextIndex];
-        setSelectedAnswerIndex(nextAnswer ? nextAnswer.selectedAnswerIndex : null); 
-        setFeedback(nextAnswer ? nextAnswer.feedback : null); 
-      }
-  };
+        setSelectedAnswerIndex(nextAnswer ? nextAnswer.selectedAnswerIndex : null);
+        setFeedback(nextAnswer ? nextAnswer.feedback : null);
+    }
+};
 
-  const handlePreviousQuestion = () => {
-      if (currentQuestionIndex > 0) {
-          const prevIndex = currentQuestionIndex - 1;
-          setCurrentQuestionIndex(prevIndex);
+const handlePreviousQuestion = () => {
+    if (currentQuestionIndex > 0) {
+        const prevIndex = currentQuestionIndex - 1;
+        setCurrentQuestionIndex(prevIndex);
 
-          // Retrieve and set the previously saved answer for the previous question
-          const prevAnswer = answers[prevIndex];
-          setSelectedAnswerIndex(prevAnswer ? prevAnswer.selectedAnswerIndex : null);
-          setFeedback(prevAnswer ? prevAnswer.feedback : null);
-      }
-  };
+        const prevAnswer = answers[prevIndex];
+        setSelectedAnswerIndex(prevAnswer ? prevAnswer.selectedAnswerIndex : null);
+        setFeedback(prevAnswer ? prevAnswer.feedback : null);
+    }
+};
 
-  const closeScoreModal = () => {
-      setShowScoreModal(false);
-  };
+const closeScoreModal = () => {
+    setShowScoreModal(false);
+};
 
-  const handleSubmitAnswer = () => {
-      if (hasCompletedQuiz) return;
+const handleSubmitAnswer = () => {
+    if (hasCompletedQuiz) return;
 
-      const isCorrect = selectedAnswerIndex === quizQuestions[currentQuestionIndex]?.correctAnswerIndex;
+    const isCorrect = selectedAnswerIndex === quizQuestions[currentQuestionIndex]?.correctAnswerIndex;
 
-      setAnswers((prevAnswers) => ({
-          ...prevAnswers,
-          [currentQuestionIndex]: {
-              selectedAnswerIndex,
-              feedback: isCorrect ? 'Correct!' : 'Incorrect.',
-          },
-      }));
+    setAnswers((prevAnswers) => ({
+        ...prevAnswers,
+        [currentQuestionIndex]: {
+            selectedAnswerIndex,
+            feedback: isCorrect ? 'Correct!' : 'Incorrect.',
+        },
+    }));
 
-      setFeedback(isCorrect ? 'Correct!' : 'Incorrect.');
+    setFeedback(isCorrect ? 'Correct!' : 'Incorrect.');
 
-      if (isCorrect) {
-          const difficulty = quizQuestions[currentQuestionIndex]?.difficulty;
-          let points = 0;
+    if (isCorrect) {
+        const difficulty = quizQuestions[currentQuestionIndex]?.difficulty;
+        let points = 0;
 
-          switch (difficulty) {
-              case 'Easy':
-                  points = 1;
-                  break;
-              case 'Medium':
-                  points = 2;
-                  break;
-              case 'Hard':
-                  points = 3;
-                  break;
-              case 'Challenging':
-                  points = 4;
-                  break;
-              default:
-                  points = 1; 
-          }
+        switch (difficulty) {
+            case 'Easy':
+                points = 1;
+                break;
+            case 'Medium':
+                points = 2;
+                break;
+            case 'Hard':
+                points = 3;
+                break;
+            case 'Challenging':
+                points = 4;
+                break;
+            default:
+                points = 1;
+        }
 
-          setScore((prevScore) => prevScore + points);
-      }
+        setScore((prevScore) => prevScore + points);
+    }
 
-      if (currentQuestionIndex === quizQuestions.length - 1) {
-          setHasCompletedQuiz(true);
-          setShowScoreModal(true);
-      }
-  };
+    if (currentQuestionIndex === quizQuestions.length - 1) {
+        setHasCompletedQuiz(true);
+        setShowScoreModal(true);
+    }
+};
 
-  return (
-      <div className="quiz-container">
-          <h1 className="quiz-title">Daily NBA Quiz</h1>
-          {hasAttempted ? (  // Conditionally render based on whether the user has already taken the quiz
-                <p>You have already taken today's quiz. Please come back tomorrow when a new quiz will be available!</p>
-          ) : quizQuestions.length > 0 ? (
-              <div>
-                  <div className="question-section">
-                      <p className="question-text">
-                          {quizQuestions[currentQuestionIndex]?.question}
-                      </p>
-                      <div className="answer-options">
-                          {quizQuestions[currentQuestionIndex]?.answers.map((answer, index) => {
+return (
+    <div className="quiz-container">
+        <h1 className="quiz-title">Daily NBA Quiz</h1>
+        {hasAttempted ? (
+            <p>You have already taken today's quiz. Please come back tomorrow when a new quiz will be available!</p>
+        ) : quizQuestions.length > 0 ? (
+            <div>
+                <div className="question-section">
+                    <p className="question-text">
+                        {quizQuestions[currentQuestionIndex]?.question}
+                    </p>
+                    <div className="answer-options">
+                        {quizQuestions[currentQuestionIndex]?.answers.map((answer, index) => {
                             const correctAnswerIndex = quizQuestions[currentQuestionIndex]?.correctAnswerIndex;
                             const isCorrect = selectedAnswerIndex === correctAnswerIndex;
-                            
+
                             return (
-                              <button
-                                  key={index}
-                                  // New logic to handle classes for correct, incorrect, and selected answers
-                                  className={`answer-button ${selectedAnswerIndex === index ? 'selected' : ''} ${feedback && index === quizQuestions[currentQuestionIndex]?.correctAnswerIndex ? 'correct' : ''} ${feedback && selectedAnswerIndex === index && !isCorrect ? 'incorrect' : ''}`}
-                                  onClick={() => handleAnswerClick(index)}
-                                  disabled={feedback !== null} 
-                              >
-                                  {answer}
-                              </button>
+                                <button
+                                    key={index}
+                                    className={`answer-button ${selectedAnswerIndex === index ? 'selected' : ''} ${feedback && index === quizQuestions[currentQuestionIndex]?.correctAnswerIndex ? 'correct' : ''} ${feedback && selectedAnswerIndex === index && !isCorrect ? 'incorrect' : ''}`}
+                                    onClick={() => handleAnswerClick(index)}
+                                    disabled={feedback !== null}
+                                >
+                                    {answer}
+                                </button>
                             );
                         })}
-                      </div>
-                  </div>
-                  {feedback && (
-                      <p className={`feedback ${feedback.toLowerCase()}`}>{feedback}</p> // Updated feedback styling logic
-                  )}
-                  <div className="action-buttons">
-                      {currentQuestionIndex > 0 && (
-                          <button className='nav-button' onClick={handlePreviousQuestion}>
-                              Previous Question
-                          </button>
-                      )}
-                      <button
-                          className="nav-button submit-button"
-                          onClick={handleSubmitAnswer}
-                          disabled={hasCompletedQuiz} 
-                      >
-                          Submit
-                      </button>
+                    </div>
+                </div>
+                {feedback && (
+                    <p className={`feedback ${feedback.toLowerCase()}`}>{feedback}</p>
+                )}
+                <div className="action-buttons">
+                    {currentQuestionIndex > 0 && (
+                        <button className='nav-button' onClick={handlePreviousQuestion}>
+                            Previous Question
+                        </button>
+                    )}
+                    <button
+                        className="nav-button submit-button"
+                        onClick={handleSubmitAnswer}
+                        disabled={hasCompletedQuiz}
+                    >
+                        Submit
+                    </button>
 
-                      {currentQuestionIndex < quizQuestions.length - 1 && (
-                          <button className="nav-button" onClick={handleNextQuestion}>
-                              Next Question
-                          </button>
-                      )}
-                  </div>
-                  {hasCompletedQuiz && (
+                    {currentQuestionIndex < quizQuestions.length - 1 && (
+                        <button className="nav-button" onClick={handleNextQuestion}>
+                            Next Question
+                        </button>
+                    )}
+                </div>
+                {hasCompletedQuiz && (
                     <p className='final-score'>Your Final Score: {score}/{totalPossibleScore}</p>
-                  )}
-              </div>
-          ) : (
-              <p>No quiz available for today. Please check back later!</p>
-          )}
-          {showScoreModal && (
-              <ScoreModal 
-                  score={score}
-                  totalQuestions={totalPossibleScore} // Display the total possible score
-                  onClose={closeScoreModal}
-              />
-          )}
-      </div>
+                )}
+            </div>
+        ) : (
+            <p>No quiz available for today. Please check back later!</p>
+        )}
+        {showScoreModal && (
+            <ScoreModal
+                score={score}
+                totalQuestions={totalPossibleScore}
+                onClose={closeScoreModal}
+            />
+        )}
+    </div>
   );
 };
 
